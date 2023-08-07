035fee3b1f594b19ea94640ec69be423485ce7ea
Johann Contreras <totocon1415@gmail.com>
2023-08-07 08:26:33 -0500

    ejercicios

---
+++ Saludador.py
@@ -0,0 +1,4 @@
+nombre=input("Cual es tu nombre: ")
+saludo="Hola,"
+pregunta="Como estas hoy?"
+print(saludo,nombre,pregunta)
---
+++ lecciones.py
@@ -0,0 +1,4 @@
+lec = int(input("Cuantas lecciones has visto?: "))
+total = 15
+faltan =  total - lec
+print("Te faltan ",faltan, "lecciones. Animo!!!")
